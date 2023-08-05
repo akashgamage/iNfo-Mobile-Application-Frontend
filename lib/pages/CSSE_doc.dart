0a34d0a6ce00f2ac2911c613334853e35abfd9d9
dula2000 <96098056+dula2000@users.noreply.github.com>
2023-08-05 21:02:46 +0530

    created CSSE Doc file

---
+++ lib/pages/CSSE_doc.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CSSEDoc extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+     
+      body: Center(
+        child: Text('This is the new page!'),
+      ),
+    );
+  }
+}
