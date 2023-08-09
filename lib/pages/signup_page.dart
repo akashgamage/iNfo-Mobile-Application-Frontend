fe087d6681063e5bb416bd44e22754d7fd562690
Sachini Anjana <97577090+NSachini@users.noreply.github.com>
2023-08-09 07:02:27 +0530

    Update Sign Up
    update the signup_page.dart

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,1 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:loginpage/pages/login_page.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formkey = GlobalKey<FormState>();
+  // Editing Controller
+  final usernameEditingController = TextEditingController();
+  final emailEditingController = TextEditingController();
+  final passwordEditingController = TextEditingController();
+
+  bool passwordObscured = true;
+
+  // Function to handle the signup
+  void _handleSignUp() {
+    if (_formkey.currentState!.validate()) {
+      // Simulate successful sign-up
+      String username = usernameEditingController.text;
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Sign Up Successful"),
+            content: Text(
+                "Congratulations, $username! Your account has been created."),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close the dialog
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                },
+                child: const Text("OK"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
