4d4c3cddd0a9012e5165f208decb608e1c7b7680
TerribleCodes <madawadesilva9998@gmail.com>
2023-08-09 12:32:27 +0530

    added notifications page

---
+++ lib/pages/Notifications/notification_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatelessWidget {
+  // Sample list of notifications
+  final List<String> notifications = [
+    'Notification 1',
+    'Notification 2',
+    'Notification 3',
+    'Notification 4',
+    'Notification 5',
+  ];
+
+  NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'NOTIFICATIONS',
+          style: TextStyle(
+            color: Color.fromARGB(255, 0, 0, 0),
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 70, 119, 225),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Row(
+            mainAxisSize: MainAxisSize.min, // Set mainAxisSize to min
+            children: [
+              Icon(Icons.arrow_back_ios, color: Colors.black),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 1.0), // Add spacing between icon and text
+                child: Text(
+                  'Back',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/bg4.png'), // Replace with your image path
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ListView.builder(
+          itemCount: notifications.length,
+          itemBuilder: (context, index) {
+            return Card(
+              color: const Color.fromARGB(255, 229, 228, 245),
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.notifications,
+                  color: Color.fromARGB(255, 30, 91, 222),
+                ),
+                title: Padding(
+                  padding: const EdgeInsets.only(bottom: 4.0),
+                  child: Text(notifications[index],
+                      style: const TextStyle(color: Colors.black)
+                          .copyWith(fontWeight: FontWeight.w700)),
+                ),
+                subtitle: const Text('This is a sample notification.'),
+                onTap: () {
+                  // Handle notification tap here if needed
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
