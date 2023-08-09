0d88b67779f8c0b25cccceb3b45f2630c94d7581
Sachini Anjana <97577090+NSachini@users.noreply.github.com>
2023-08-09 08:11:09 +0530

    create sign up

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:info_mobile_application/pages/home_page.dart';
+import 'pages/signup_page.dart';
 
 void main() => runApp(
-  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => MyApp(), // Wrap your app
-  ),
-);
+      DevicePreview(
+          enabled: !kReleaseMode,
+          builder: (context) => MyApp(),
+          builder: (context) => SignUp() // Wrap your app
+          ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
