631d905fc05cedcbe19c934d5f349d278a77d983
Thanumiii <jatkperera@students.nsbm.ac.lk>
2023-08-19 13:52:36 +0530

    Created DS department

---
+++ lib/pages/Departments1/Dep_DS.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:departments1/pages/info_card.dart';
+
+class DS extends StatelessWidget {
+  const DS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        // Wrap the content in SingleChildScrollView
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24.0),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Heading Image
+                Image.asset(
+                  'assets/DS/DS.jpg',
+                  fit: BoxFit.cover,
+                  height: 210,
+                  width: 150,
+                ),
+                const SizedBox(height: 8.0),
+                // Info Cards
+                ListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: const [
+                    InfoCard(
+                      imagePath: 'assets/CSSE/dean-foc.jpg',
+                      description: 'Dr. Rasika Ranaweera',
+                      additionalText: 'Dean - Faculty of Computing',
+                    ),
+                    InfoCard(
+                      imagePath: 'assets/DS/mr.pramudya.jpg',
+                      description: 'Mr. Pramudya Thilakarathne',
+                      additionalText: 'Head / Lecturer',
+                    ),
+                    InfoCard(
+                      imagePath: 'assets/DS/dr.chaminda.jpg',
+                      description: 'Dr. Chaminda Wijesinghe',
+                      additionalText: 'Senior Lecturer',
+                    ),
+                    InfoCard(
+                      imagePath: 'assets/DS/ms.nethmi.jpg',
+                      description: 'Ms. Nethmi Weerasingha',
+                      additionalText: 'Lecturer',
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                // Close Button
+                /*
+                ElevatedButton(
+                  onPressed: () {
+                    // To close the popup on button click.
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Close'),
+                ),
+                */
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
